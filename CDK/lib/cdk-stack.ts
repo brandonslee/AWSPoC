c0b11ee782359cb8f68f5da253c69ab92bd91afc
Brandon S. Lee <acidzazz@gmail.com>
2020-07-27 16:53:55 +0900

    AppSync creation added, explicit role names used. #4

--- CDK/lib/cdk-stack.ts
+++ CDK/lib/cdk-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from '@aws-cdk/core';
 import * as iam from '@aws-cdk/aws-iam';
 import * as dynamodb from '@aws-cdk/aws-dynamodb';
+import * as appsync from '@aws-cdk/aws-appsync';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -13,6 +14,7 @@ export class CdkStack extends cdk.Stack {
     // AppSync to DynamoDB
     const role = new iam.Role(this, "MBPDynamoDBAccess", {
       assumedBy: new iam.ServicePrincipal('appsync.amazonaws.com'),
+      roleName: 'MBPDynamoDBAccess',  // explicit role name
     });
 
     let policy = new iam.Policy(this, "DynamoDBAccess", {
@@ -42,6 +44,7 @@ export class CdkStack extends cdk.Stack {
     // AppSync to CloudWatch
     const role2 = new iam.Role(this, 'MBPCloudWatchLogging', {
       assumedBy: new iam.ServicePrincipal('appsync.amazonaws.com'),
+      roleName: 'MBPCloudWatchLogging', // explicit role name
     })
     role2.addManagedPolicy(iam.ManagedPolicy.fromManagedPolicyArn(this, 'ManagedPolicy', 
       'arn:aws:iam::aws:policy/service-role/AWSAppSyncPushToCloudWatchLogs'));
@@ -65,6 +68,14 @@ export class CdkStack extends cdk.Stack {
       readCapacity: 5,
       writeCapacity: 5,
     });
+
+    // AppSync creation
+    // Level1 Cfn used as Level2 is experimental at the moment
+    const graphQLApi = new appsync.CfnGraphQLApi(this, 'MBP API', {
+      authenticationType: 'API_KEY',
+      name: 'MBP_API',
+    });
+
     
   }
 }
